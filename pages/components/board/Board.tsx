7086c41d5e7a4c065f37786256c32d7db20b962b
Alex <zthh626@gmail.com>
2021-12-02 16:40:45 -0800

    Add board component and player types

---
+++ pages/components/board/Board.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import React, { useState } from "react";
+import Player from "../../types/Player";
+
+function Board() {
+  const [board, setBoard] = useState<Player[][]>([[]]);
+  return <div></div>;
+}
+
+export default Board;
---
+++ pages/types/Player.tsx
@@ -0,0 +1,3 @@
+type Player = "PlayerOne" | "PlayerTwo";
+
+export default Player;
