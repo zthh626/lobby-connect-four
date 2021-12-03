852e41c46c911705a1360c4e6188f6a474bbe4ff
Alex <zthh626@gmail.com>
2021-12-02 16:40:33 -0800

    Add rules modal

---
+++ pages/components/modals/rules/RulesModal.tsx
@@ -0,0 +1,63 @@
+import { Button, Text } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  ModalFooter,
+} from "@chakra-ui/modal";
+import React from "react";
+
+interface RulesModalProps {
+  isOpen: boolean;
+  onClose: () => void;
+}
+
+function RulesModal({ isOpen, onClose }: RulesModalProps) {
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader>Connect Four Rules</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody>
+          <Text px="0.3em" pt="0.3em" pb="1em">
+            <Text fontWeight="bold">Game objective</Text>Be the first player to
+            connect 4 of the same colored discs in a row (either vertically,
+            horizontally, or diagonally)
+            <hr />
+          </Text>
+
+          <Text p="0.3em">
+            First, decide who goes first and what color each player will have. -
+            Players must alternate turns, and only one disc can be dropped in
+            each turn.
+          </Text>
+          <br />
+          <Text p="0.3em">
+            On your turn, drop one of your colored discs from the top into any
+            of the seven slots.
+          </Text>
+          <br />
+          <Text p="0.3em">
+            The game ends when there is a 4-in-a-row or a stalemate.
+          </Text>
+          <br />
+          <Text p="0.3em">
+            The starter of the previous game goes second on the next game.
+          </Text>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={onClose}>
+            Close
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+}
+
+export default RulesModal;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,45 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Flex,
+  Center,
+  Heading,
+  Button,
+  HStack,
+  useDisclosure,
+} from "@chakra-ui/react";
 import type { NextPage } from "next";
+import React, { useState } from "react";
+import Board from "./components/board/Board";
+import RulesModal from "./components/modals/rules/RulesModal";
 
-const Home: NextPage = () => {
-  return <Flex h="100vh"></Flex>;
+const ConnectFourPage: NextPage = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Flex justifyContent="center" h="100vh">
+      {isPlaying ? (
+        <Board />
+      ) : (
+        <Center flexDir="column">
+          <Heading p="1em ">Connect Four</Heading>
+          <HStack>
+            <>
+              <Button onClick={onOpen}>Rules</Button>
+
+              <RulesModal isOpen={isOpen} onClose={onClose} />
+            </>
+            <Button
+              onClick={() => {
+                setIsPlaying((prev) => !prev);
+              }}
+            >
+              Start Game
+            </Button>
+          </HStack>
+        </Center>
+      )}
+    </Flex>
+  );
 };
 
-export default Home;
+export default ConnectFourPage;
